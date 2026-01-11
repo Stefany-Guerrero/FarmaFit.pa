c60ec5d450f92e7a989e470c35e099ec2378decd
Stefany-Guerrero <stefanyrachel11@gmail.com>
2026-01-11 15:28:55 -0500

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,29 +1,17 @@
-// ===============================
-// LOGIN SIMPLE (LOCAL)
-// ===============================
+<form id="login-form" class="login-form">
+  <input
+    type="email"
+    id="login-email"
+    placeholder="Correo electrónico"
+    required
+  >
 
-const form = document.getElementById("login-form");
+  <input
+    type="password"
+    id="login-password"
+    placeholder="Contraseña"
+    required
+  >
 
-if (form) {
-  form.addEventListener("submit", function (e) {
-    e.preventDefault();
-
-    const email = document.getElementById("login-email").value;
-    const password = document.getElementById("login-password").value;
-
-    // Validación básica
-    if (!email || !password) {
-      alert("Completa todos los campos");
-      return;
-    }
-
-    // Guardar sesión (simulada)
-    localStorage.setItem("usuario", JSON.stringify({
-      email: email
-    }));
-
-    // Ir al home
-    window.location.href = "home.html";
-  });
-}
-
+  <button type="submit" class="btn">Entrar</button>
+</form>
